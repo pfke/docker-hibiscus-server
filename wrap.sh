599a4fa1fdb48a6eae6115868225d57ca93bed33
Martin Kleine <Martin Kleine>
2017-06-02 20:30:58 +0200

    adding docker-compose

--- wrap.sh
+++ wrap.sh
@@ -11,6 +11,7 @@ case "$DB_DRIVER" in
 
 # Be sure to escape a ':' in DB_ADDR
 mysql)
+    DB_ADDR=${DB_HOSTNAME}:${DB_PORT-3306}
     echo "Configuring MySQL db connection to $DB_ADDR"
     cat > $dbconfig <<EOF
 database.driver=de.willuhn.jameica.hbci.server.DBSupportMySqlImpl
@@ -21,6 +22,7 @@ EOF
     ;;
 
 postgres)
+    DB_ADDR=${DB_HOSTNAME}:${DB_PORT-5432}
     echo "Configuring PostgreSQL db connection to $DB_ADDR"
     cat > $dbconfig <<EOF
 database.driver=de.willuhn.jameica.hbci.server.DBSupportPostgreSQLImpl
@@ -53,30 +55,47 @@ EOF
 
 
 function initdb() {
-    IFS=':' read -ra ADDR <<< "$DB_ADDR"
-    DB_HOST=${ADDR[0]}
-    DB_PORT=${ADDR[1]}
+    #echo "Initializing database, please wait a second ..."
+    #sleep 1
+    echo "Initializing ..."
 
     case "$DB_DRIVER" in
         postgres)
-            cmd="psql -h $DB_HOST -p $DB_PORT -U $DB_USERNAME  < /hibiscus-server/plugins/hibiscus/sql/postgresql-create.sql"
+            cmd="psql -h $DB_HOSTNAME -p ${DB_PORT-5432} -U $DB_USERNAME  < /hibiscus-server/plugins/hibiscus/sql/postgresql-create.sql"
             echo $cmd
             eval PGPASSWORD=$DB_PASSWORD $cmd
             ;;
 
         mysql)
-            echo "initdb for driver mysql still needs to be added - go make a pull request"
-            ;;
+            cmd="mysql -u $DB_USERNAME --password=${DB_PASSWORD} -P ${DB_PORT-3306} -h $DB_HOSTNAME $DB_NAME < /hibiscus-server/plugins/hibiscus/sql/mysql-create.sql"
+            echo $cmd
+            eval $cmd 
+                        ;;
         *)
             echo "Don't know how to initialize database for driver $DB_DRIVER"
             ;;
     esac
+    echo "Initialization complete!"
+    
 }
 
 if [ "$*" == "initdb" ]; then
         initdb
         exit
 fi
+
+
+initdb
+
+# FILE=initialized.flag
+# if [ ! -f $FILE ]; then
+#    initdb
+#    touch $FILE
+#    exit
+# else
+#    echo "Already initalized!"
+# fi
+
 
 # Write configuration file based on desired database driver
 ${@-/hibiscus-server/jameicaserver.sh -p $PASSWORD -f /srv/hibiscus}
